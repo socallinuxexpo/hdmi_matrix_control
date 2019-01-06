8ee229979f9af48152da8fd357be4ab5725667c1
M Starch <lestarch@googlemail.com>
2019-01-05 20:40:15 -0800

    lestarch: pylint fixes

--- hw/matrix.py
+++ hw/matrix.py
@@ -8,84 +8,67 @@ Contains the root functionality for the matrix driver and related components.
 import time
 import threading
 import logging
-import random
-import simplejson as json
-from time import sleep
+
 
 class MatrixDriver(threading.Thread):
-  '''
-  Base class of the matrix driver component. Provides a running thread and basic interactivity
-  for generic matrix switches.
-  '''
-  def __init__(self, inputs=4, outputs=4, name="MatrixDriver"):
     '''
-    Construct the matirx driver and associated thread.
-    @param inputs: number of inputs
-    @param outputs: number of outputs
-    @param name: name to provide the thread
+    Base class of the matrix driver component. Provides a running thread and basic interactivity
+    for generic matrix switches.
     '''
-    threading.Thread.__init__(self, name=name)
-    self.inputs = inputs
-    self.outputs = outputs
-    #inputs and output checks
-    assert inputs >= 1, "Expected integral number of inputs"
-    assert outputs >= 1, "Expected integral number of outputs"
-    self.channels = [1] *  outputs #All inputs start mapped to 1
 
-  def setup():
-    '''
-    Setup function.
-    '''
-    logging.debug("Setting up Matrix")
+    def __init__(self, inputs=4, outputs=4, name="MatrixDriver"):
+        '''
+        Construct the matirx driver and associated thread.
+        @param inputs: number of inputs
+        @param outputs: number of outputs
+        @param name: name to provide the thread
+        '''
+        threading.Thread.__init__(self, name=name)
+        self.inputs = inputs
+        self.outputs = outputs
+        # inputs and output checks
+        assert inputs >= 1, "Expected integral number of inputs"
+        assert outputs >= 1, "Expected integral number of outputs"
+        self.channels = [1] * outputs  # All inputs start mapped to 1
+        self.running = True
 
-  def assign(out_chan, in_chan):
-    '''
-    Assign a given input channel to a given output channel
-    @param out_chan: output channel
-    @param in_chan: input channel
-    '''
-    logging.debug("Assigning %d to %s", (out_chan, in_chan))
-    assert out_chan < self.outputs, "Invalid output channel %d" % out_chan
-    assert in_chan < self.inputs, "Invalid input channel %d" % in_chan
-    self.channels[out_chan] = in_chan
+    def setup(self):
+        '''
+        Setup function.
+        '''
+        logging.debug("Setting up Matrix")
 
-  def read(out_chan):
-    '''
-    Reads what a current output is set to.
-    @param out_chan: output channel to read
-    '''
-    logging.debug("Request to read %d", out_chan)
-    assert out_chan < self.outputs, "Invalid output channel %d" % out_chan
-    return self.channels[out_chan]
+    def assign(self, out_chan, in_chan):
+        '''
+        Assign a given input channel to a given output channel
+        @param out_chan: output channel
+        @param in_chan: input channel
+        '''
+        logging.debug("Assigning %d to %s", out_chan, in_chan)
+        assert out_chan < self.outputs, "Invalid output channel %d" % out_chan
+        assert in_chan < self.inputs, "Invalid input channel %d" % in_chan
+        self.channels[out_chan] = in_chan
 
-  def loop(self):
-    '''
-    Run one loop iteration
-    '''
-    logging.debug("Iterating loop")
+    def read(self, out_chan):
+        '''
+        Reads what a current output is set to.
+        @param out_chan: output channel to read
+        '''
+        logging.debug("Request to read %d", out_chan)
+        assert out_chan < self.outputs, "Invalid output channel %d" % out_chan
+        return self.channels[out_chan]
 
-  def run(self):
-    '''
-    Main thread/looping function
-    '''
-    self.setup()
-    while self.running:
-      self.loop()
-      self.sleep(0.1)
+    def loop(self):
+        '''
+        Run one loop iteration
+        '''
+        logging.debug("Iterating loop")
 
-#  def toJSON(self):
-#    test = {}
-#    test['name'] = self.name
-#    test['config'] = {}
-#    test['config']['inputs'] = self.num_inputs
-#    test['config']['outputs'] = self.num_outputs
-#    test['outputs'] = {}
-#    for output in range(1, self.num_outputs+1):
-#      test['outputs']["output_{}".format(output)] = self.getOutput(output)
-#    return json.dumps(test, sort_keys=True)
-
-#if __name__ == "__main__":
-#  serial_port = '/dev/ttyUSB0'
-#  mat = MatrixDriver(serial_port)
-#  print( mat.toJSON() )
-
+    def run(self):
+        '''
+        Main thread/looping function
+        '''
+        self.setup()
+        while self.running:
+            self.loop()
+            time.sleep(0.1)
--- hw/tesmart.py
+++ hw/tesmart.py
@@ -5,74 +5,57 @@ Module containing TESmart matix driver code, for interacting with the TESmart ma
 
 @author mproctor13
 '''
-import serial
+import re
 import time
-import threading
 import logging
-import random
+import serial
+
 import hw.matrix
 
 
 class TESmartMatrix(hw.matrix.MatrixDriver):
-  '''
-  TESmartMatrix driver used to drive the TESmart matrix.
-  '''
-  def __init__(self, port, name="TESmartMatrix"):
     '''
-    Open up the serial port owning this driver
-    @param port: port string to open up
-    @param name: name of the thread
+    TESmartMatrix driver used to drive the TESmart matrix.
     '''
-    super().__init__(inputs=4, outputs=4, name=name)
-    self.port = serial.Serial(port)
+    OUT_REG = re.compile(r"O\dI(\d)")
+    def __init__(self, port, name="TESmartMatrix"):
+        '''
+        Open up the serial port owning this driver
+        @param port: port string to open up
+        @param name: name of the thread
+        '''
+        super().__init__(inputs=4, outputs=4, name=name)
+        self.port = serial.Serial(port)
 
-  def setup():
-    '''
-    Initial setup for this matrix. Send out the initial write.
-    '''
-    self.serial_port.write(b'MT00RD0000NT')
-    
-  def loop(self):
-    '''
-    Runs one iteration of the loop to process this driver
-    '''
-    data = self.serial_port.read(48)
-    if len(data) == 48:
-        self.channels = str(data)
-    else:
-        logging.warning("Unexpected message: %s", data)
-    super().loop()
+    def setup(self):
+        '''
+        Initial setup for this matrix. Send out the initial write.
+        '''
+        self.port.write(b'MT00RD0000NT')
 
-  def assign(out_chan, in_chan):
-    '''
-    Assign a given input channel to a given output channel
-    @param out_chan: output channel
-    @param in_chan: input channel
-    '''
-    logging.debug("Assigning %d to %s", (out_chan, in_chan))
-    assert out_chan < self.outputs, "Invalid output channel %d" % out_chan
-    assert in_chan < self.inputs, "Invalid input channel %d" % in_chan
-    self.port.write(b"MT00SW%02d%02dNT" % (out_chan, in_chan))
+    def loop(self):
+        '''
+        Runs one iteration of the loop to process this driver
+        '''
+        data = self.port.read(48)
+        if len(data) == 48:
+            data = str(data)
+            matches = self.OUT_REG.find_all(data)
+            self.channels = [int(match.group(1)) for match in matches][:self.outputs]
+        else:
+            logging.warning("Unexpected message: %s", data)
+        super().loop()
 
-  def getOutput(self, port):
-    # LINK:O1I2;O2I2;O3I2;O4I1;O5I2;O6I1;O7I9;O8I2;END
-    start = 5+(5*(port-1))
-    logging.debug( "port=[%s] part=[%s]" % (port, str(self.status)[start+5]) ) 
-    return int(self.status[start+5])
-
-  @staticmethod
-  def outputString(outport, inport):
-
-  def setOutput(self, outport, inport):
-    logging.debug( "setOutput got set outport %s to inport %s" % (outport, inport) )
-    self.serial_port.write(TESmartMatrix.outputString(outport,inport))
-    time.sleep(0.1)
-    self.serial_port.write(b'MT00RD0000NT')
-    time.sleep(0.1)
-    
-  def countUp(self):
-    self.serial_port.write(b'MT00SW0000NT')
-    time.sleep(0.1)
-    self.serial_port.write(b'MT00RD0000NT')
-    time.sleep(0.1)
-
+    def assign(self, out_chan, in_chan):
+        '''
+        Assign a given input channel to a given output channel
+        @param out_chan: output channel
+        @param in_chan: input channel
+        '''
+        logging.debug("Assigning %d to %s", out_chan, in_chan)
+        assert out_chan < self.outputs, "Invalid output channel %d" % out_chan
+        assert in_chan < self.inputs, "Invalid input channel %d" % in_chan
+        self.port.write(b"MT00SW%02d%02dNT" % out_chan, in_chan)
+        time.sleep(0.1)
+        self.port.write(b'MT00RD0000NT')
+        time.sleep(0.1)
