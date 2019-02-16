f062dcc3159c5890fa64487ed4b51b6819314a15
Carlos Meza <carlos@digitalr00ts.com>
2019-02-16 01:02:22 -0800

    tmp.py cleanup

--- tests/tmp.py
+++ tests/tmp.py
@@ -1,42 +1,48 @@
-import time
-import threading
-import hdmi_matrix_controller.hw.scalemat
+# pylint: disable=missing-docstring
 import logging
+import threading
+import time
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger.addHandler(logging.StreamHandler())
+import hdmi_matrix_controller.hw.scalemat
 
-sleep_wait = 7
 
-logger.debug("Initializing driver")
-driver = hdmi_matrix_controller.hw.scalemat.ScaleMatrix("/dev/ttyUSB0")
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+LOGGER.addHandler(logging.StreamHandler())
 
-logger.debug("Waiting %s", sleep_wait)
-time.sleep(sleep_wait)
+SLEEP_WAIT = 7
 
-logger.debug("Setting up driver")
-driver.start()
+LOGGER.debug("Initializing driver")
+DRIVER = hdmi_matrix_controller.hw.scalemat.ScaleMatrix("/dev/ttyUSB0")
+
+LOGGER.debug("Waiting %s", SLEEP_WAIT)
+time.sleep(SLEEP_WAIT)
+
+LOGGER.debug("Setting up driver")
+DRIVER.start()
+
 
 def runner():
-    #for i in [0, 1, 2, 3]:
+    # for i in [0, 1, 2, 3]:
     #    driver.assign(i, 0)
 
     for i in [0, 1, 2, 3]:
         for j in [0, 1, 2, 3]:
-            logger.debug("Setting %s,%s", i + 1, j + 1)
-            driver.assign(i, j)
+            LOGGER.debug("Setting %s,%s", i + 1, j + 1)
+            DRIVER.assign(i, j)
             time.sleep(1)
-            port_status = driver.read(i)
-            #print(j + " = " +port_status)
+            port_status = DRIVER.read(i)
+            # print(j + " = " +port_status)
             assert j == port_status
-            #assert read == j, "Read wrongo %d vs %d" % (read, j)
+            # assert read == j, "Read wrongo %d vs %d" % (read, j)
             time.sleep(1)
+
 
 class Tmp(threading.Thread):
     def run(self):
         runner()
-        driver.running = False
+        DRIVER.running = False
 
-two = Tmp()
-two.start()
+
+TWO = Tmp()
+TWO.start()
