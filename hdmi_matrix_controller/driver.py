6fb0724b9ec2838ae81478a6f51b3940a9f2431e
M Starch <lestarch@googlemail.com>
2019-10-30 19:37:42 -0700

    mstarch: preparing matrix backer for Flask

--- hdmi_matrix_controller/driver.py
+++ hdmi_matrix_controller/driver.py
@@ -1,2 +1,8 @@
 """ Driver constant"""
-DRIVER = None
+import serial
+from . import hw
+try:
+    serial_port = serial.Serial("/dev/ttyUSB0")  # open serial port
+    DRIVER = hw.TESmartMatrix(serial_port)
+except:
+    DRIVER = hw.MatrixDriver()
